8d37108c879675e6ab455156fabbe640b014dac4
Janis Lebron <janislebron@gmail.com>
2020-05-26 18:17:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,11 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition,
+  attr_accessor :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def brand
+    @brand
   end
 end
